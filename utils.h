18e2745b5eabf88804f7d5115e5e4431ae442d3c
shankar <shankar@64e4521d-a91b-dd11-9388-003048836090>
2008-05-13 01:14:13 +0000

    added init and valid functions for dead_negative_or_unit_clauses
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/private/mcsat/trunk@17 64e4521d-a91b-dd11-9388-003048836090

--- utils.h
+++ utils.h
@@ -371,6 +371,7 @@ typedef struct clause_table_s {
   samp_clause_t *dead_clauses;//list of unselected clauses
   samp_clause_t *negative_or_unit_clauses; //list of negative weight or unit 
                                            //clauses for fast unit propagation
+  samp_clause_t *dead_negative_or_unit_clauses; //killed clauses
   samp_clause_t *sat_clauses; //list of fixed satisfied clauses
 } clause_table_t;
 
