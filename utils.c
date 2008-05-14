f599ef4f55329803cd6c6adf1675f406fece558b
owre <owre@64e4521d-a91b-dd11-9388-003048836090>
2008-05-14 10:53:32 +0000

    Added round argument to print_state
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/private/mcsat/trunk@47 64e4521d-a91b-dd11-9388-003048836090

--- utils.c
+++ utils.c
@@ -683,12 +683,18 @@ void print_clause_table(samp_table_t *table, int32_t num_vars){
 }
 
 
-void print_state(samp_table_t *table){
+void print_state(samp_table_t *table, uint32_t round){
   atom_table_t *atom_table = &(table->atom_table);
-  printf("==============================================================\n");
-  print_atoms(table);
-  printf("==============================================================\n");
-  print_clause_table(table, atom_table->num_vars);
+  if (verbosity_level > 2) {
+    printf("==============================================================\n");
+    print_atoms(table);
+    printf("==============================================================\n");
+    print_clause_table(table, atom_table->num_vars);
+  } else if (verbosity_level > 1) {
+    printf("Generated sample %d...\n", round);
+  } else if (verbosity_level > 0) {
+    printf(".");
+  }
 }
 
 void init_rule_table(rule_table_t *table){
