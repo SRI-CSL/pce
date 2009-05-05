2ca66ca4637ba3995b72900ce4750035ab548b67
saadati <saadati@64e4521d-a91b-dd11-9388-003048836090>
2009-05-05 22:27:02 +0000

    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/private/mcsat/trunk@217 64e4521d-a91b-dd11-9388-003048836090

--- xpce_client/src/java/com/sri/csl/xpce/xmlrpc/XpceClient.java
+++ xpce_client/src/java/com/sri/csl/xpce/xmlrpc/XpceClient.java
@@ -7,25 +7,27 @@ import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
+import com.sri.csl.exception.XPCException;
 import com.sri.csl.xpce.json.XPCEConstants;
 
 public class XpceClient {
 	
 	protected URL xpceServerURL;
+	protected XmlRpcClient xmlrpcClient = null;
 	
-	public void setServerURL(String url) {
+	public XpceClient(String serverURL) {
 	    try {
-	    	xpceServerURL = new URL(url);
+	    	xpceServerURL = new URL(serverURL);
+		    XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+	    	config.setServerURL(xpceServerURL);
+		    xmlrpcClient = new XmlRpcClient();
+		    xmlrpcClient.setConfig(config);
 	    } catch (MalformedURLException m) {
 	    	m.printStackTrace();
 	    }
 	}
 	
-	public URL getServerURL() {
-		return xpceServerURL;
-	}
-	
-	public XmlRpcClient getXmlRpcClient() {
+	protected XmlRpcClient getXmlRpcClient() {
 	    XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
     	config.setServerURL(xpceServerURL);
 	    XmlRpcClient xmlrpcClient = new XmlRpcClient();
@@ -33,20 +35,32 @@ public class XpceClient {
 		return xmlrpcClient;
 	}
 	
-	public Object command(String command) throws XmlRpcException {
+	public Object command(String command) throws XmlRpcException, XPCException {
     	return getXmlRpcClient().execute(XPCEConstants.XPCECOMMAND, new Object[] {command});
 	}
 	
+	public void addSort(String sort) throws XmlRpcException, XPCException {
+		// TODO: This will later be replaced with a JSON-based command
+		command("sort " + sort + ";");
+	}
+	
+	public void addSubsort(String sort, String superSort) throws XmlRpcException, XPCException {
+		// TODO: This will later be replaced with a JSON-based command
+		command("subsort " + sort + " " + superSort + ";");
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
-	    XpceClient client = new XpceClient();
-    	client.setServerURL("http://localhost:8080/RPC2");
+	    XpceClient client = new XpceClient("http://localhost:8080/RPC2");
     	Object ret;
 	    try {
-	    	ret = client.command("sort S;");
-	    	System.out.println("Return Value:" + ret);
+	    	client.addSort("S");
 	    	client.command("const a, b, c, d, e: S;");
 	    	client.command("predicate P(S) indirect;");
-	    	client.command("predicate Q(S) indirect;");
+	    	ret = client.command("predicate Q(S) indirect;");
+	    	System.out.println("Return Value:" + ret);
 	    	client.command("add P(a) 2.0;");
 	    	client.command("add P(c) 2.0;");
 	    	client.command("add P(e) 2.0;");
@@ -57,7 +71,7 @@ public class XpceClient {
 	    	client.command("add ~P(d) or Q(d) 3.0;");
 	    	client.command("add ~P(e) or Q(e) 3.0;");
 	    	client.command("mcsat 0.01, 20.0, 0.01, 30, 500;");
-	    }catch (XmlRpcException e) {
+	    }catch (Exception e) {
 	    	e.printStackTrace();
 	    }
 	}
