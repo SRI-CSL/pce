d3cefe295fc0f4226828ed7bded82e7a4583ab7b
saadati <saadati@64e4521d-a91b-dd11-9388-003048836090>
2009-06-02 22:54:24 +0000

    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/private/mcsat/trunk@233 64e4521d-a91b-dd11-9388-003048836090

--- xpce_client/src/java/com/sri/csl/xpce/json/XPCERespond.java
+++ xpce_client/src/java/com/sri/csl/xpce/json/XPCERespond.java
@@ -5,24 +5,23 @@ import org.json.JSONObject;
 
 import com.sri.csl.exception.XPCException;
 
-public class Result {
+public class XPCERespond {
 	protected String err = null, warn = null;
 	protected Object result = null;
 	protected boolean successful = true;
 	
 	
-	public Result(JSONObject obj) throws JSONException, XPCException {
+	public XPCERespond(JSONObject obj) throws JSONException, XPCException {
 		if ( obj == JSONObject.NULL ) return; // Successful call.
 		if ( obj.has(XPCEConstants.XPCEERROR) ) {
 			successful = false;
 			err = obj.getString(XPCEConstants.XPCEERROR);
-			throw new XPCException(err);
+			throw new XPCException("XPCE SERVER ERROR: " + err);
 		}
 		if ( obj.has(XPCEConstants.XPCEWARNING) ) {
 			warn = obj.getString(XPCEConstants.XPCEWARNING);
 			System.out.println(warn);
 		}
-		// TODO: result should be translated from JSON to the proper object
 		if ( obj.has(XPCEConstants.XPCERESULT) )
 			result = obj.get(XPCEConstants.XPCERESULT);
 	}
