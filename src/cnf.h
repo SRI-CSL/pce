71a6c274d4cacdca7e0e018ba1194da6cf083f89
owre <owre@64e4521d-a91b-dd11-9388-003048836090>
2011-01-10 11:58:14 +0000

    Minor change
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/private/mcsat/trunk@373 64e4521d-a91b-dd11-9388-003048836090

--- src/cnf.h
+++ src/cnf.h
@@ -4,7 +4,7 @@
 
 extern pvector_t ask_buffer;
 
-extern void add_cnf(char **fixed, input_formula_t *formula, double weight, char *source, bool add_weights);
+extern void add_cnf(char **frozen, input_formula_t *formula, double weight, char *source, bool add_weights);
 
 extern void ask_cnf(input_formula_t *formula, double threshold, int32_t numresults);
 
